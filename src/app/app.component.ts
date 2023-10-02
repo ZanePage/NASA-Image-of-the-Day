d9664719bb3e10baaf334ee2c287664538be3502
ZanePage <zaneapage@gmail.com>
2023-10-02 11:56:49 -0400

    Integrated NASA Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NasaApiService } from './nasa-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'niotd';
+export class AppComponent implements OnInit {
+  title = 'NASA IMAGE OF THE DAY';
+
+  copyright: string = "";
+  date: string = "";
+  explanation: string = "";
+  url: string = "";
+  title_img: string = "";
+
+  constructor(private api: NasaApiService) { }
+
+  ngOnInit() {
+    this.api.getPost().subscribe(
+      (response) => {
+        console.log(response);
+        this.date = response['date'];
+        this.copyright = response['copyright'];
+        this.explanation = response['explanation'];
+        this.url = response['hdurl'];
+        this.title_img = response['title'];
+      },
+      (error) => { console.log(error); }
+    );
+  }
 }
--- src/app/nasa-api.service.ts
+++ src/app/nasa-api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NasaApiService {
 
-  constructor() { }
+  apiUrl: string = 'https://api.nasa.gov/planetary/apod?api_key=8v1JxargfrYToSSj9E3gWX43SaNIOvsMeJMOZoiD';
+
+  constructor(private http: HttpClient) { }
+
+  getPost(): Observable<any>{
+    return this.http.get(this.apiUrl);
+  }
 }
